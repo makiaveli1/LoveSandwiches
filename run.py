22cfbd394a327846c482e21832cf18285d8847c9
makiaveli1 <itsmakiaveli@gmail.com>
2023-09-14 14:46:32 +0100

    Add user requesr loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from google.oauth2.service_account import Credentials
 
 
 SCOPE = [
-"https://www.googleapis.com/auth/spreadsheets",
-"https://www.googleapis.com/auth/drive.file",
-"https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
 ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
@@ -25,25 +25,29 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
-    
-    data_str = input("Enter your data here: ")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
 
-    
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
+
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
-    
+
     """
-    
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -52,6 +56,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+        
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
